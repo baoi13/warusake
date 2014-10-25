ba74ef3b91c2101435a4e374c588d781b14bed54
NAKAGAKI Kenji <nakaken0629@gmail.com>
2014-10-25 16:10:23 +0900

    tweetだけでなく、汎用的にシェアできるように修正

--- app/src/main/java/org/gtugs/tokai/warusake/MixActivity.java
+++ app/src/main/java/org/gtugs/tokai/warusake/MixActivity.java
@@ -3,9 +3,10 @@ package org.gtugs.tokai.warusake;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,8 +15,8 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -59,7 +60,7 @@ public class MixActivity extends Activity {
     public static class PlaceholderFragment extends Fragment {
         private TextView firstTextView;
         private TextView secondTextView;
-        private ImageButton tweetButton;
+        private ImageButton shareButton;
 
         private Handler handler = new Handler();
 
@@ -68,22 +69,20 @@ public class MixActivity extends Activity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_mix, container, false);
             this.firstTextView = (TextView) rootView.findViewById(R.id.firstTextView);
             this.secondTextView = (TextView) rootView.findViewById(R.id.secondTextView);
-            this.tweetButton = (ImageButton) rootView.findViewById(R.id.tweetButton);
+            this.shareButton = (ImageButton) rootView.findViewById(R.id.shareButton);
 
             this.firstTextView.setText(randomText());
             this.secondTextView.setText("");
-            this.tweetButton.setVisibility(View.INVISIBLE);
+            this.shareButton.setVisibility(View.INVISIBLE);
 
-            this.tweetButton.setOnClickListener(new View.OnClickListener() {
+            this.shareButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    String url = "http://twitter.com/share?text=" + tweetText();
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                    startActivity(intent);
+                    share();
                 }
             });
 
@@ -106,22 +105,46 @@ public class MixActivity extends Activity {
                     @Override
                     public void run() {
                         PlaceholderFragment.this.secondTextView.setText(randomText());
-                        PlaceholderFragment.this.tweetButton.setVisibility(View.VISIBLE);
+                        PlaceholderFragment.this.shareButton.setVisibility(View.VISIBLE);
                     }
                 });
             }
         }
 
-        private String tweetText() {
+        private String makeShareText() {
             String first = this.firstTextView.getText().toString();
             String second = this.secondTextView.getText().toString();
             String text = first + "を" + second + "で割って飲みま〜す #warusake";
 
-            try {
-                return URLEncoder.encode(text, "utf-8");
-            } catch (UnsupportedEncodingException e) {
-                return null;
+            return text;
+        }
+
+        private void share() {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+
+            List<ResolveInfo> resInfo = getActivity().getPackageManager().queryIntentActivities(intent, 0);
+            if (resInfo.isEmpty()) {
+                return;
             }
+
+            String shareText = makeShareText();
+            ArrayList<Intent> shareIntentList = new ArrayList<Intent>();
+
+            for (ResolveInfo info : resInfo) {
+                Intent shareIntent = (Intent) intent.clone();
+                if (info.activityInfo.packageName.toLowerCase().equals("com.facebook.katana")) {
+                    /* facebookは文字をシェアできないので、対象から外す */
+                } else {
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+                    shareIntent.setPackage(info.activityInfo.packageName);
+                    shareIntentList.add(shareIntent);
+                }
+            }
+
+            Intent chooserIntent = Intent.createChooser(shareIntentList.remove(0), "友だちに伝える");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, shareIntentList.toArray(new Parcelable[]{}));
+            startActivity(chooserIntent);
         }
     }
 }
